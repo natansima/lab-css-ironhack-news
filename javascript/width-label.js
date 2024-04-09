c6ed4df1cde414da312e6378f7bfe865aed1f743
Natan Simoes <natan16simoes@gmail.com>
2024-04-09 23:40:37 +0100

    IRONHACK NEWS BY NATAN

--- javascript/width-label.js
+++ javascript/width-label.js
@@ -4,7 +4,6 @@ const viewportDimensions = document.querySelector(".viewport-dimensions");
 const width = window.innerWidth;
 // Set the text of the element to show the viewport width
 viewportDimensions.innerHTML = `Viewport Width: <b>${width}px</b>`;
-
 window.addEventListener("resize", () => {
   // Get the viewport width
   const width = window.innerWidth;
